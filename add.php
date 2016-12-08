7d15e126d4fc601487d281c66d04a836c14a3acc
Justin Stewart <stewartjustind@gmail.com>
2016-12-08 11:45:40 -0800

    Updated query to insert lat/long values to database

--- add.php
+++ add.php
@@ -5,9 +5,11 @@
 
 	$temp1=$_POST["temp1"];
 	$hum1=$_POST["hum1"];
+	$lat=$_POST["lat"];
+	$long=$_POST["long"];
 
-	$query = "INSERT INTO `tempLog` (`temperature`, `humidity`) 
-		VALUES ('".$temp1."','".$hum1."')"; 
+	$query = "INSERT INTO `HUMIDITY` (`temperature`, `humidity`, 'longitude', 'latitude') 
+		VALUES ('".$temp1."','".$hum1."', '".$lat."', '".$long."')"; 
    	
    	mysql_query($query,$link);
 	mysql_close($link);
